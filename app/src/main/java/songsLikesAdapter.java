86f3a9ee47ab8d9380da4394dcfdd653e9d15e35
danielpale <62296781+danielpale@users.noreply.github.com>
2021-06-20 11:19:57 -0500

    Se creo primera parte de la funcionalidad de ver song like

--- app/src/main/java/songsLikesAdapter.java
+++ app/src/main/java/songsLikesAdapter.java
@@ -1,20 +1,36 @@
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.feimusic.R;
+import com.example.feimusic.Response.CancionResponse;
 
 import java.util.ArrayList;
 
 public class songsLikesAdapter extends BaseAdapter {
-    //private ArrayList<>
+    private Context context;
+    private ArrayList<CancionResponse> songLikeList;
+
+    public songsLikesAdapter(Context context, ArrayList<CancionResponse> songLikeList) {
+        this.context = context;
+        this.songLikeList = songLikeList;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return songLikeList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return songLikeList.get(position);
     }
 
     @Override
@@ -24,6 +40,28 @@ public class songsLikesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        CancionResponse song = (CancionResponse) getItem(position);
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.songlike_list_item, null);
+        ImageView imgViewSongLike = convertView.findViewById(R.id.imgViewSongLike);
+        TextView txtSongName = convertView.findViewById(R.id.txtsongName);
+        Bitmap imagen = StringToBitMap(song.getImagenCancion());
+
+        imgViewSongLike.setImageBitmap(imagen);
+        txtSongName.setText(song.getNombreCancion());
+
+        return convertView;
+    }
+
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte = Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }
+        catch(Exception e){
+            e.getMessage();
+            return null;
+        }
     }
 }
